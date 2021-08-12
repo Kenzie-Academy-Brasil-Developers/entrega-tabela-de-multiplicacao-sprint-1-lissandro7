3af75ca20eeca5f2578faefc8f552a6eb0f66cfa
lissandro <lissandroperf7@gmail.com>
2021-08-12 12:26:55 -0300

    Enviando Resolução da Tabela de Multiplicação

---
+++ sandrao.js
@@ -0,0 +1,16 @@
+function multiplos(norte) {
+    const armazenarOne = [];
+
+    for (let c = 0; c <= norte; c++) {
+        const armazenarTwo = [];
+
+        for (let i = 0; i <= norte; i++) {
+            const sul = i * c;
+            armazenarTwo.push(sul);
+
+        }
+        armazenarOne.push(armazenarTwo);
+    }
+
+    console.table(armazenarOne);
+}
