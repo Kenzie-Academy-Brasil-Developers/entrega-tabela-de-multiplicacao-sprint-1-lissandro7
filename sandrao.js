2c200a90668d0baf7954e0aba6cbd4be529056c4
lissandro <lissandroperf7@gmail.com>
2021-08-13 10:55:32 -0300

    Esqueci de Retornar a Função,Resolvido

--- sandrao.js
+++ sandrao.js
@@ -3,14 +3,14 @@ function multiplos(norte) {
 
     for (let c = 0; c <= norte; c++) {
         const armazenarTwo = [];
-
+        
         for (let i = 0; i <= norte; i++) {
             const sul = i * c;
             armazenarTwo.push(sul);
-
         }
         armazenarOne.push(armazenarTwo);
-    }
-
+    } 
     console.table(armazenarOne);
+    return armazenarOne;
 }
+multiplos(7);
